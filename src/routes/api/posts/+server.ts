cd7f77d33bc40675338c02d6ed834b9c7c29b4c9
Ian Price <ianprice943@yahoo.com>
2023-02-15 16:17:45 -0500

    first round of PUT and POST api endpoints finished

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,6 +1,63 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from "./$types";
+import { supabase } from '$lib/utils/supabaseClient';
+
+export const PUT = (async ({ request }) => {
+    const { 
+        is_published,
+        published_at,
+        urlStub,
+        title,
+        description,
+        content 
+    } = await request.json();
+    
+    const { data, error } = await supabase
+    .from('blog_posts')
+    .update({
+        'is_published': is_published,
+        'published_at': published_at,
+        'title': title,
+        'description': description,
+        'content': content
+    })
+    .eq('urlStub', urlStub);
+
+    if(!error) {
+        console.log("PUT data", data);
+        return json("ok");
+    } else {
+        return json("update failed");
+    }
+
+}) satisfies RequestHandler;
 
 export const POST = (async ({ request }) => {
-    
+    const { 
+        is_published,
+        published_at,
+        urlStub,
+        title,
+        description,
+        content 
+    } = await request.json();
+
+    const { data, error } = await supabase
+    .from('blog_posts')
+    .insert([
+        {'is_published': is_published},
+        {'published_at': published_at},
+        {'urlStub': urlStub},
+        {'title': title},
+        {'description': description},
+        {'content': content}
+    ]);
+
+    if(!error) {
+        console.log("POST data", data);
+        return json("ok");
+    } else {
+        return json("insert failed");
+    }
+
 }) satisfies RequestHandler;
