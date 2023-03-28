ccbd79d7282163329c7fb52933721193746ecf1e
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-28 18:10:44 -0400

    Removed another test file that's going to need an integration test

--- src/lib/components/PCard/PCard.test.ts
+++ src/lib/components/PCard/PCard.test.ts
@@ -1,8 +1,15 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
+import PCard from './PCard.svelte';
 
-describe("", async () => {
-    it('', async () => {
+describe("PCard.svelte", async () => {
+    const result = render(PCard, {cardContent: "Test string"});
+    const card = result.container.querySelector('p');
 
+    it('Should render a paragraph', async () => {
+        expect(card).not.toBeNull();
+    });
+    it('Should render "Test string" inside the paragraph', () => {
+        expect(card?.innerHTML).toBe("Test string")
     });
 });
