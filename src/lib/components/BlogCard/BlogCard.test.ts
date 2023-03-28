30c7f2b7c45890dcb909bba656d25fa6f3afb636
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-28 11:25:13 -0400

    adding more tests

--- src/lib/components/BlogCard/BlogCard.test.ts
+++ src/lib/components/BlogCard/BlogCard.test.ts
@@ -1,1 +1,36 @@
 import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import BlogCard from './BlogCard.svelte';
+
+describe('BlogCard.svelte', async () => {
+    const result = render(BlogCard, 
+        {
+            urlStub: 'test', 
+            title: 'test title',
+            description: 'a description',
+            thumbnailUrl: 'https://www.google.com',
+            thumbnailAltText: 'the picture'
+        }
+    );
+    const card = result.container;
+
+    it('Should render an article wrapped in a list item', async () => {
+        expect(card.querySelector('li > article')).not.toBeNull();
+    });
+    it('Should render a link with the href of "/blog/test"', async () => {
+        const anchor = card.querySelector('a');
+        expect(anchor).not.toBeNull();
+        expect(anchor?.href).toContain('/blog/test');
+    });
+    it('Should render an image with a src of "https://www.google.com/"', async () => {
+        const image = card.querySelector('img');
+        expect(image).not.toBeNull();
+        expect(image?.src).toBe('https://www.google.com/');
+        expect(image?.alt).toBe('the picture');
+    });
+    it('Should have an h2 with the content of "test title"', async () => {
+        const h2 = card.querySelector('h2');
+        expect(h2).not.toBeNull();
+        expect(h2?.innerText).toBe('test title');
+    });
+})
