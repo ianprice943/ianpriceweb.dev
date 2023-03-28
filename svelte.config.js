c1b4dceb961804ba27426bd858f15fd3a59d2af0
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-28 18:56:53 -0400

    changed adapter to netlify, added netlify.toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,10 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: false,
+			split: false
+		}),
 	},
 	extensions: [
 		'.svelte',
