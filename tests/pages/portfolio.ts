60b44b68e1eedac68d809b327a9edc584521ecc0
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-28 18:18:56 -0400

    added h1 checks to /portfolio and /blog

--- tests/pages/portfolio.ts
+++ tests/pages/portfolio.ts
@@ -1,1 +1,6 @@
 import { expect, test } from '@playwright/test';
+
+test('portfolio page has expected h1', async ({ page }) => {
+	await page.goto('/portfolio');
+	expect(await page.textContent('h1')).toBe('Portfolio');
+});
