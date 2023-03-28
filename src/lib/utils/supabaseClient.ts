418b5a2b6440996a0ccbe4bf67752699ae355227
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-28 16:20:47 -0400

    remove unused type and commented line from supabase client. added more tests to utils.test.ts

--- src/lib/utils/supabaseClient.ts
+++ src/lib/utils/supabaseClient.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import type { Database } from '$lib/types/database.types'
 import { env } from '$env/dynamic/public'
 
-// export const supabase = createClient<Database>(`${env.SUPABASE_URL}`, `${env.SUPABASE_KEY}`)
 export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_KEY);
--- src/lib/utils/utils.test.ts
+++ src/lib/utils/utils.test.ts
@@ -1,15 +1,57 @@
 import { describe, it, expect } from 'vitest';
-import { convertDateString } from './utils';
+import { screen } from '@testing-library/svelte';
+import { getTodayString, convertDateString, highlightSettings, setTabIndexOnCodeBlocks } from './utils';
+
+describe('highlight.js settings', () => {
+	it('should return highlight.js converted html with typescript highlighting', async () => {
+		const lang = 'typescript';
+		const code = "const test = 'Hello World'";
+		expect(highlightSettings(code, lang)).toContain('hljs');
+	});
+	it('should return highlight.js converted html with no highlighting', () => {
+		const lang = '';
+		const code = "const test2 = 'No highlighting'";
+		expect(highlightSettings(code, lang)).not.toContain('hljs');
+	});
+});
 
 describe('Date Formatting', () => {
-	it('expects March 28th 2023 to be formatted to 2023/3/28', () => {
+	it('should get today\'s date in YYYY/M(M)/D(D) format', () => {
+		const dateObj = new Date();
+		const month = dateObj.getUTCMonth() + 1;
+		const day = dateObj.getUTCDate();
+		const year = dateObj.getUTCFullYear();
+		const today = `${year}/${month}/${day}`;
+		
+		expect(
+			getTodayString()
+		).toBe(today);
+	});
+	it('should have March 28th 2023 be formatted to 2023/3/28', () => {
 		expect(
             convertDateString('March 28 2023')
         ).toBe("2023/3/28");
 	});
-	it('expects 2023-03-05 00:00:00+00 to be formatted to 2023/3/5', () => {
+	it('should have 2023-03-05 00:00:00+00 be formatted to 2023/3/5', () => {
 		expect(
 			convertDateString('2023-03-05 00:00:00+00')
 		).toBe("2023/3/5");
 	});
+});
+
+describe('setTabIndexOnCodeBlocks', () => {
+	it('should add tabIndex=0 to all code tags', () => {
+		for(let i = 0; i < 5; i++) {
+			const codeNode = document.createElement('code');
+			codeNode.innerHTML = 'test';
+			document.body.appendChild(codeNode);
+		}
+		setTabIndexOnCodeBlocks(true);
+
+		const nodes = screen.getAllByText('test');
+
+		nodes.forEach((node) => {
+			expect(node.tabIndex).toBe(0);
+		})
+	})
 });
