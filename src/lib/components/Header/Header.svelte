ebc5b74c8a4ff40fb05fd0eaa1b13eb8c6a96cc0
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-29 11:41:48 -0400

    update nav spacing

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -7,18 +7,18 @@
     <div class="sm:flex-grow">
         <p class="text-4xl text-center sm:text-left">Ian Price</p>
     </div>
-    <nav class="flex text-lg sm:flex-grow-0">
+    <nav class="flex text-lg py-2 sm:py-0 sm:flex-grow-0">
         <ul class="flex mx-auto flex-row sm:my-auto">
-            <li class="pr-2 hover:underline {$page?.route?.id === "/" ? "underline" : ""}">
+            <li class="px-1 hover:underline {$page?.route?.id === "/" ? "underline" : ""}">
                 <a href="/">Home</a>
             </li>
-            <li class="pr-2 hover:underline {$page?.route?.id === "/resume" ? "underline" : ""}">
+            <li class="px-1 hover:underline {$page?.route?.id === "/resume" ? "underline" : ""}">
                 <a href="/resume">Resume</a>
             </li>
-            <li class="pr-2 hover:underline {$page?.route?.id === "/portfolio" ? "underline" : ""}">
+            <li class="px-1 hover:underline {$page?.route?.id === "/portfolio" ? "underline" : ""}">
                 <a href="/portfolio">Portfolio</a>
             </li>
-            <li class="sm:pr-2 hover:underline {$page?.route?.id?.includes("blog") ? "underline" : ""}">
+            <li class="px-1 sm:pr-2 hover:underline {$page?.route?.id?.includes("blog") ? "underline" : ""}">
                 <a href="/blog">Blog</a>
             </li>
         </ul>
