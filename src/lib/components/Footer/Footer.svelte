505c02405cc64754a38f925cee8985d9540d3e87
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-29 11:34:34 -0400

    remove aria-hidden from link

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -5,9 +5,9 @@
 <footer class="text-center p-2 flex-grow-0 flex-shrink-0 relative">
     <div class="justify-self-center">Copyright © 2023 - Ian Price</div>
     {#if !$page.data.session}
-        <a aria-hidden="true" href="/login" class="underline sm:absolute sm:right-6 sm:top-2">Admin Login</a>
+        <a href="/login" class="underline sm:absolute sm:right-6 sm:top-2">Admin Login</a>
     {:else}
-        <form aria-hidden="true" method="POST" action="/login?/logout">
+        <form method="POST" action="/login?/logout">
             <button on:submit|preventDefault type="submit" class="underline sm:absolute sm:right-6 sm:top-2">Admin Logout</button>
         </form>
     {/if}
