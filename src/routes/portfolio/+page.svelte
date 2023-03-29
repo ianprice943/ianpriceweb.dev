e17c49498bc05fb9efeaf34c794ab907d8408c59
Ian Price <ianprice@Ians-MacBook-Pro.local>
2023-03-29 18:25:59 -0400

    style update to make the links surrounding portfolio cards full height if the picture is short

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -19,22 +19,22 @@
 <section class="grid md:grid-cols-2 gap-4">
     <article class="flex flex-col shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">To see what side projects I&apos;m working on, check out my GitHub</h2>
-        <a href="https://github.com/ianprice943" aria-label="Ian's GitHub" class="my-auto rounded-xl bg-gray-600 dark:bg-transparent">
-            <img src="/images/GitHub_Logo_White.webp" alt="GitHub Logo" />
+        <a href="https://github.com/ianprice943" aria-label="Ian's GitHub" class="flex h-full justify-center rounded-xl bg-gray-600 dark:bg-transparent">
+            <img src="/images/GitHub_Logo_White.webp" alt="GitHub Logo" class="my-auto" />
         </a>
     </article>
     <article class="shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">Three.js Playground</h2>
         <p class="text-center text-sm py-2">A small playground I developed to learn the basics of Three.js.</p>
-        <a href="https://ip-threejs-playground.netlify.app/" aria-label="A website showing off some basics of Three.js" class="flex y-auto justify-center">
+        <a href="https://ip-threejs-playground.netlify.app/" aria-label="A website showing off some basics of Three.js" class="flex my-auto justify-center">
             <img src="/images/three.webp" alt="Three.js rendered sphere" class='rounded-xl' />
         </a>
     </article>
     <article class="flex flex-col shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">William Kampf Mediation</h2>
         <p class="text-center text-sm py-2">An introductory site and blog for a client&apos;s Mediation business.</p>
-        <a href="https://wkmediation.com/" aria-label="Website for a mediator in Los Angeles" class="my-auto">
-            <img src="/images/wk_mediation_logo_horiz_graygold.webp" alt="WK Mediation Logo" />
+        <a href="https://wkmediation.com/" aria-label="Website for a mediator in Los Angeles" class="flex h-full justify-center">
+            <img src="/images/wk_mediation_logo_horiz_graygold.webp" alt="WK Mediation Logo" class="my-auto" />
         </a>
     </article>
     <article class="shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
