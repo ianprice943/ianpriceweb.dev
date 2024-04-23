c3cd603db7b7b5d4bdaa5017a6d6592877fe5700
Ian Price <ianprice943@yahoo.com>
2024-04-23 11:38:38 -0400

    update footer code to use current year at all times

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import { page } from '$app/stores';
+
+    const year = new Date().getFullYear()
 </script>
 
 <footer class="text-center p-2 flex-grow-0 flex-shrink-0 relative">
-    <div class="justify-self-center">Copyright © 2023 - Ian Price</div>
+    <div class="justify-self-center">Copyright © { year } - Ian Price</div>
     {#if !$page.data.session}
         <a href="/login" class="underline sm:absolute sm:right-6 sm:top-2">Admin Login</a>
     {:else}
