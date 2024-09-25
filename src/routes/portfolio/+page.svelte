54746ec262ff2d0aee26f75bd69516440d78ffd8
Ian Price <ianprice943@yahoo.com>
2024-09-25 09:50:00 -0400

    added OVArts to portfolio

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -19,36 +19,43 @@
 <div class="grid md:grid-cols-2 gap-4">
     <article class="flex flex-col shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">To see what side projects I&apos;m working on, check out my GitHub</h2>
-        <a href="https://github.com/ianprice943" aria-label="Ian's GitHub" class="flex h-full justify-center rounded-xl bg-gray-600 dark:bg-transparent">
+        <a href="https://github.com/ianprice943" target="_blank" aria-label="Ian's GitHub, this link opens in a new tab" class="flex h-full justify-center rounded-xl bg-gray-600 dark:bg-transparent">
             <img src="/images/GitHub_Logo_White.webp" alt="GitHub Logo" class="my-auto" />
+        </a>
+    </article>
+    <article class="flex flex-col shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
+        <h2 class="text-center text-lg font-semibold">Operation Valor Arts</h2>
+        <p class="text-center text-sm py-2">Operation Valor Arts is a 501(c)(3) dedicated to providing Veterans with training and resources to design interpretive public art.</p>
+        <a href="https://www.ovarts.org" target="_blank" aria-label="The website for Operation Valor Arts, this link opens in a new tab" class="flex h-full justify-center rounded-xl">
+            <img src="/images/OVArts-Logo.webp" alt="GitHub Logo" class="my-auto" />
         </a>
     </article>
     <article class="shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">Three.js Playground</h2>
         <p class="text-center text-sm py-2">A small playground I developed to learn the basics of Three.js.</p>
-        <a href="https://ip-threejs-playground.netlify.app/" aria-label="A website showing off some basics of Three.js" class="flex my-auto justify-center">
+        <a href="https://ip-threejs-playground.netlify.app/" target="_blank" aria-label="A website showing off some basics of Three.js, this link opens in a new tab" class="flex my-auto justify-center">
             <img src="/images/three.webp" alt="Three.js rendered sphere" class='rounded-xl' />
         </a>
     </article>
     <article class="flex flex-col shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">William Kampf Mediation</h2>
         <p class="text-center text-sm py-2">An introductory site and blog for a client&apos;s Mediation business.</p>
-        <a href="https://wkmediation.com/" aria-label="Website for a mediator in Los Angeles" class="flex h-full justify-center">
+        <a href="https://wkmediation.com/" target="_blank" aria-label="Website for a mediator in Los Angeles, this link opens in a new tab" class="flex h-full justify-center">
             <img src="/images/wk_mediation_logo_horiz_graygold.webp" alt="WK Mediation Logo" class="my-auto" />
         </a>
     </article>
     <article class="shadow-lg rounded-xl p-4 lg:p-2 xl:p-4 border-gray-50 border-4 dark:bg-gray-600 dark:border-gray-600">
         <h2 class="text-center text-lg font-semibold">Pokemon Mystery Dungeon Rescue Team DX Starters</h2>
         <p class="text-center text-sm py-2">A website I developed to learn Vue.js, and later React.js + Typescript (Note, due to Heroku removing their free tier of hosting services, these sites are now offline. Links now take you to their respective GitHub repositories.)</p>
-        <a href="https://github.com/ianprice943/PMDDXStarters-react" aria-label="Link to React.js Version Source Code" class="flex my-auto justify-center">
+        <a href="https://github.com/ianprice943/PMDDXStarters-react" target="_blank" aria-label="Link to React.js Version Source Code, this link opens in a new tab" class="flex my-auto justify-center">
             <img src="/images/PMDRTDXLogo.webp" alt="Pokemon Mystery Dungeon Rescue Team DX Logo" />
         </a>
         <ul class="flex justify-around pt-2">
             <li class="text-center hover:underline">
-                <a href="https://github.com/ianprice943/PMDDXStarters">Vue.js Version Source Code</a>
+                <a href="https://github.com/ianprice943/PMDDXStarters" target="_blank" aria-label="Link to Vue.js Version Source Code, this link opens in a new tab">Vue.js Version Source Code</a>
             </li>
             <li class="text-center hover:underline">
-                <a href="https://github.com/ianprice943/PMDDXStarters-react">React.js Version Source Code</a>
+                <a href="https://github.com/ianprice943/PMDDXStarters-react" target="_blank" aria-label="Link to React.js Version Source Code, this link opens in a new tab">React.js Version Source Code</a>
             </li>
         </ul>
     </article>
